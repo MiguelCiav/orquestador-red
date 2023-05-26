14f3201044b7535ef062ce3182e7d2bc35511436
Miguel Ciavato <miguelciavato@gmail.com>
2023-05-25 23:17:11 -0400

    Estructuras de datos
    1. Se añadió un constructor a la clase dispositivo y a la clase relacion
    
    2. Se establecieron atributos privados y públicos para la clase relacion y dispositivo, junto con funciones get( ); para cada atributo privado.
    
    3. Se creó la clase ListaDispositivo y la clase ListaRelaciones ambas usadas como reemplazo a la estructura "Lista"

--- Estructuras PROYECTO.cpp
+++ Estructuras PROYECTO.cpp
@@ -5,7 +5,147 @@
 #include <string>
 using namespace std;
 
+//SECCION_1: CLASE Dispositivo
 
+class Dispositivo{
+    private:
+        string hostname;
+        string ip; 
+
+    public:
+        //Relacion* relaciones; POR SOLUCIONAR
+        Dispositivo *siguiente;
+        Dispositivo *anterior;
+
+        Dispositivo(string hostname, string ip);
+        string getHostname();
+        string getIp();
+};
+
+Dispositivo::Dispositivo(string hostname, string ip){
+    this->hostname = hostname;
+    this->ip = ip;
+    siguiente = nullptr;
+    anterior = nullptr;
+}
+
+string Dispositivo::getHostname(){
+    return hostname;
+}
+
+string Dispositivo::getIp(){
+    return ip;
+}
+
+//SECCION_2: CLASE Relacion
+
+class Relacion{
+    private:
+        string tipoDeConexion; 
+        int ping;
+
+    public:
+        Dispositivo *destino;
+        Relacion *siguiente;
+        Relacion *anterior;
+
+        Relacion(string tipoDeConexion, int ping);
+        string getTipoDeConexion();
+        int getPing();
+};
+
+Relacion::Relacion(string tipoDeConexion, int ping){
+    this->tipoDeConexion = tipoDeConexion;
+    this->ping = ping;
+    anterior = nullptr;
+    siguiente = nullptr;
+}
+
+string Relacion::getTipoDeConexion(){
+    return tipoDeConexion;
+}
+
+int Relacion::getPing(){
+    return ping;
+}
+
+//SECCION_3: CLASE ListaDispositivo
+
+class ListaDispositivo{
+    private:
+        Dispositivo* lista;
+    
+    public:
+        void insertarElemento(string hostname, string ip);
+        void mostrarListado();
+        ListaDispositivo();
+};
+
+ListaDispositivo::ListaDispositivo(){
+    lista = nullptr;
+}
+
+void ListaDispositivo::insertarElemento(string hostname, string ip){
+    Dispositivo *nuevoDispositivo = new Dispositivo(hostname, ip);
+
+    if(lista == nullptr){
+        lista = nuevoDispositivo;
+    } else {
+        nuevoDispositivo->siguiente = lista;
+        lista->anterior = nuevoDispositivo;
+        lista = nuevoDispositivo;
+    }
+}
+
+void ListaDispositivo::mostrarListado(){
+    Dispositivo* actual = lista;
+
+    while(actual != nullptr){
+        cout<<actual->getHostname()<<" ";
+        cout<<actual->getIp()<<endl;
+        actual = actual->siguiente;
+    }
+}
+
+//SECCION_4: CLASE ListaRelaciones
+
+class ListaRelaciones{
+    private:
+        Relacion* lista;
+    
+    public:
+        void insertarElemento(string tipoDeConexion, int ping);
+        void mostrarListado();
+        ListaRelaciones();
+};
+
+ListaRelaciones::ListaRelaciones(){
+    lista = nullptr;
+}
+
+void ListaRelaciones::insertarElemento(string tipoDeConexion, int ping){
+    Relacion *nuevaRelacion = new Relacion(tipoDeConexion, ping);
+
+    if(lista == nullptr){
+        lista = nuevaRelacion;
+    } else {
+        nuevaRelacion->siguiente = lista;
+        lista->anterior = nuevaRelacion;
+        lista = nuevaRelacion;
+    }
+}
+
+void ListaRelaciones::mostrarListado(){
+    Relacion* actual = lista;
+
+    while(actual != nullptr){
+        cout<<actual->getTipoDeConexion()<<" ";
+        cout<<actual->getPing()<<endl;
+        actual = actual->siguiente;
+    }
+}
+
+//SECCION 5: CLASE Utilitaria
 
 class Utilitaria{
     Dispositivo Dispositivos;
@@ -31,43 +171,12 @@ class Utilitaria{
     }
 };
 
-class Dispositivo{
-    string hostname;
-    string ip;
-    Relacion relaciones; 
-    Dispositivo *siguiente;
-    Dispositivo *anterior; 
-};
+int main(){
+    ListaDispositivo dispositivos;
 
-class Relacion{
-    int tipoDeConexion; 
-    int ping;
-    Dispositivo *destino;
-    Relacion *siguiente;
-    Relacion *anterior;
-};
+    dispositivos.insertarElemento("ABC","192.168.0.1");
+    dispositivos.insertarElemento("DEF","192.168.1.1");
+    dispositivos.mostrarListado();
 
-class Lista {
-    Lista *siguiente;
-    Lista *anterior;
-
-    void MostrarElemento (){
-
-    }
-
-    void eliminarElemento(){
-
-    }
-
-    void insertarElemento(){
-
-    }
-
-    void buscarElemento(){
-
-    }
-
-    void ordenar(){
-
-    }
-};
+    return 0;
+}
