d0fb54b0918bb58281ca3eb5d1c924a30faf1961
Miguel Ciavato <miguelciavato@gmail.com>
2023-05-29 22:54:21 -0400

    commit
    efef

--- Estructuras PROYECTO.cpp
+++ Estructuras PROYECTO.cpp
@@ -81,7 +81,108 @@ int Relacion::getPing(){
     return ping;
 }
 
-//SECCION_3: CLASE ListaDispositivo
+//SECCION_3: CLASE ListaRelaciones
+
+class ListaRelaciones{
+    public:
+        Relacion* lista;
+        void insertarElemento(string tipoDeConexion, int ping, Dispositivo* destino);
+        void mostrarListado();
+        bool verificarExistencia(Dispositivo* destino);
+        Relacion* buscarRelacion(Dispositivo* destino);
+        void eliminarRelacion(Dispositivo *destino);
+        Relacion* buscarRelacion(Dispositivo* destino);
+        ListaRelaciones();
+};
+
+ListaRelaciones::ListaRelaciones(){
+    lista = nullptr;
+}
+
+void ListaRelaciones::insertarElemento(string tipoDeConexion, int ping, Dispositivo* destino){
+    Relacion *nuevaRelacion = new Relacion(tipoDeConexion, ping, destino);
+
+    if(lista == nullptr){
+        lista = nuevaRelacion;
+    } else {
+        nuevaRelacion->siguiente = lista;
+        lista->anterior = nuevaRelacion;
+        lista = nuevaRelacion;
+    }
+}
+
+void ListaRelaciones::mostrarListado(){
+    Relacion* actual = lista;
+
+    while(actual != nullptr){
+        cout<<actual->destino->getHostname()<<" ";
+        cout<<actual->getTipoDeConexion()<<" ";
+        cout<<actual->getPing()<<endl;
+        actual = actual->siguiente;
+    }
+}
+
+bool ListaRelaciones::verificarExistencia(Dispositivo* destino){
+
+    Relacion* auxiliar = lista;
+
+    while(auxiliar != nullptr){
+        if(auxiliar->destino->getHostname() == destino->getHostname()){
+            return true;
+        }
+        auxiliar = auxiliar->siguiente;
+    }
+
+    return false;
+}
+
+Relacion* ListaRelaciones::buscarRelacion(Dispositivo* destino){
+
+    Relacion* auxiliar = lista;
+
+    while(auxiliar != nullptr){
+        if(auxiliar->destino->getHostname() == destino->getHostname()){
+            return auxiliar;
+        }
+        auxiliar = auxiliar->siguiente;
+    }
+
+    return nullptr;
+};
+
+void ListaRelaciones::eliminarRelacion(Dispositivo *destino){
+
+    Relacion* aux = buscarRelacion(destino);
+
+    if(aux->anterior != nullptr && aux->siguiente != nullptr){
+
+        aux->anterior->siguiente = aux->siguiente;
+        aux->siguiente->anterior = aux->anterior;
+        
+    }
+
+    if(aux->anterior == nullptr && aux->siguiente != nullptr){
+
+        aux->siguiente->anterior = nullptr;
+        
+    }
+
+    if(aux->anterior != nullptr && aux->siguiente == nullptr){
+
+        aux->anterior->siguiente = nullptr;
+        
+    }
+
+    if(lista == aux){
+
+        lista = aux->siguiente;
+
+    }
+
+    delete aux;
+}
+
+//SECCION_4: CLASE ListaDispositivo
 
 class ListaDispositivo{
     private:
@@ -89,7 +190,9 @@ class ListaDispositivo{
     
     public:
         void insertarElemento(string hostname, string ip);
+        void crearRelacion(Dispositivo* A, Dispositivo* B, int ping, string tipoDeConexion);
         void mostrarListado();
+        void eliminarDispositivo(Dispositivo *Dispositivo);
         Dispositivo* buscarPorHostname(string hostname);
         Dispositivo* buscarPorIP(string ip);
         ListaDispositivo();
@@ -97,6 +200,24 @@ class ListaDispositivo{
 
 ListaDispositivo::ListaDispositivo(){
     lista = nullptr;
+}
+
+void ListaDispositivo::eliminarDispositivo(Dispositivo *Dispositivo){
+
+    if(Dispositivo->anterior != nullptr && Dispositivo->siguiente != nullptr){
+
+        Dispositivo->anterior->siguiente = Dispositivo->siguiente;
+        Dispositivo->siguiente->anterior = Dispositivo->anterior;
+        
+    }
+
+    if(lista == Dispositivo){
+
+        lista = Dispositivo->siguiente;
+
+    }
+
+    delete Dispositivo;
 }
 
 void ListaDispositivo::insertarElemento(string hostname, string ip){
@@ -147,41 +268,15 @@ Dispositivo* ListaDispositivo::buscarPorIP(string ip){
     return nullptr;
 }
 
-//SECCION_4: CLASE ListaRelaciones
+void ListaDispositivo::crearRelacion(Dispositivo* A, Dispositivo* B, int ping, string tipoDeConexion){
 
-class ListaRelaciones{
-    public:
-        Relacion* lista;
-        void insertarElemento(string tipoDeConexion, int ping, Dispositivo* destino);
-        void mostrarListado();
-        ListaRelaciones();
-};
+    if(!A->relaciones->verificarExistencia(B) && !B->relaciones->verificarExistencia(A)){
 
-ListaRelaciones::ListaRelaciones(){
-    lista = nullptr;
-}
+        A->relaciones->insertarElemento(tipoDeConexion,ping,B);
+        B->relaciones->insertarElemento(tipoDeConexion,ping,A);
 
-void ListaRelaciones::insertarElemento(string tipoDeConexion, int ping, Dispositivo* destino){
-    Relacion *nuevaRelacion = new Relacion(tipoDeConexion, ping, destino);
-
-    if(lista == nullptr){
-        lista = nuevaRelacion;
-    } else {
-        nuevaRelacion->siguiente = lista;
-        lista->anterior = nuevaRelacion;
-        lista = nuevaRelacion;
     }
-}
 
-void ListaRelaciones::mostrarListado(){
-    Relacion* actual = lista;
-
-    while(actual != nullptr){
-        cout<<actual->destino->getHostname()<<" ";
-        cout<<actual->getTipoDeConexion()<<" ";
-        cout<<actual->getPing()<<endl;
-        actual = actual->siguiente;
-    }
 }
 
 //SECCION 5: BACKTRACKING
@@ -296,13 +391,6 @@ void BuscarRuta(Dispositivo* origen, Dispositivo* destino, pilaDispositivos &pil
 
 }
 
-void crearRelacion(Dispositivo *&Dispositivo1, Dispositivo *&Dispositivo2, int ping, string conexion){
-
-    Dispositivo1->relaciones->insertarElemento(conexion,ping,Dispositivo2);
-    Dispositivo2->relaciones->insertarElemento(conexion,ping,Dispositivo1);
-
-}
-
 //SECCION 6: CLASE Utilitaria
 
 class Utilitaria{
@@ -331,7 +419,46 @@ class Utilitaria{
 
 int main(){
 
+    ListaDispositivo dispositivos;
 
+    //PROCEDIMIENTO PARA CREAR NUEVOS DISPOSITIVOS
+
+    dispositivos.insertarElemento("D1","192.168.1.1");
+    dispositivos.insertarElemento("D2","192.168.0.1");
+    dispositivos.insertarElemento("D3","192.168.1.1");
+    dispositivos.insertarElemento("D4","192.168.1.1");
+
+    //PROCEDIMIENTO PARA CREAR LISTAS DE RELACIONES EN DISPOSITIVOS
+
+    dispositivos.buscarPorHostname("D1")->relaciones = new ListaRelaciones;
+    dispositivos.buscarPorHostname("D2")->relaciones = new ListaRelaciones;
+    dispositivos.buscarPorHostname("D3")->relaciones = new ListaRelaciones;
+    dispositivos.buscarPorHostname("D4")->relaciones = new ListaRelaciones;
+
+    //PROCEDIMIENTO PARA CREAR RELACIONES ENTRE DISPOSITIVOS
+
+    dispositivos.crearRelacion(dispositivos.buscarPorHostname("D1"), dispositivos.buscarPorHostname("D2"), 124, "5G");
+    dispositivos.crearRelacion(dispositivos.buscarPorHostname("D2"), dispositivos.buscarPorHostname("D3"), 124, "5G");
+    dispositivos.crearRelacion(dispositivos.buscarPorHostname("D3"), dispositivos.buscarPorHostname("D4"), 124, "5G");
+
+    //SALIDA:
+
+    cout<<"DISPOSITIVOS: "<<endl<<endl;
+    dispositivos.mostrarListado();
+    cout<<endl;
+
+    cout<<"RELACIONES: "<<endl<<endl;
+    cout<<"- D1:"<<endl;
+    dispositivos.buscarPorHostname("D1")->relaciones->mostrarListado();
+
+    cout<<endl<<"- D2:"<<endl;
+    dispositivos.buscarPorHostname("D2")->relaciones->mostrarListado();
+
+    cout<<endl<<"- D3:"<<endl;
+    dispositivos.buscarPorHostname("D3")->relaciones->mostrarListado();
+
+    cout<<endl<<"- D4:"<<endl;
+    dispositivos.buscarPorHostname("D4")->relaciones->mostrarListado();
 
     return 0;
 }
